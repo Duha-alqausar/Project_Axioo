f6f6a6c3f4ac057abd8a51df0f00e6733eae165d
Duha-alqausar <44722172+Duha-alqausar@users.noreply.github.com>
2019-05-08 09:22:06 +0700

    Add files via upload

--- Absensi_p/db_karyawan.sql
+++ Absensi_p/db_karyawan.sql
@@ -1,20 +1,22 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.11
--- http://www.phpmyadmin.net
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 07 Mei 2019 pada 04.26
--- Versi Server: 5.6.21
--- PHP Version: 5.6.3
+-- Waktu pembuatan: 08 Bulan Mei 2019 pada 04.21
+-- Versi server: 10.1.38-MariaDB
+-- Versi PHP: 7.3.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `db_karyawan`
@@ -26,21 +28,21 @@ SET time_zone = "+00:00";
 -- Struktur dari tabel `absensi`
 --
 
-CREATE TABLE IF NOT EXISTS `absensi` (
-`id_absensi` int(11) NOT NULL,
+CREATE TABLE `absensi` (
+  `id_absensi` int(11) NOT NULL,
   `id_karyawan` int(11) DEFAULT NULL,
   `tanggal_absen` date NOT NULL,
   `jam_masuk` time NOT NULL,
   `keterangan` text NOT NULL,
   `jam_keluar` time DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data untuk tabel `absensi`
 --
 
 INSERT INTO `absensi` (`id_absensi`, `id_karyawan`, `tanggal_absen`, `jam_masuk`, `keterangan`, `jam_keluar`) VALUES
-(23, 3, '2019-05-01', '14:33:32', 'Hadir', '09:14:45'),
+(23, 3, '2019-05-01', '14:33:32', 'Hadir', '08:39:48'),
 (24, 2, '2019-05-06', '14:33:58', 'Hadir', '08:42:24'),
 (33, 1, '2019-05-07', '09:23:13', 'Hadir', '09:23:29');
 
@@ -50,18 +52,18 @@ INSERT INTO `absensi` (`id_absensi`, `id_karyawan`, `tanggal_absen`, `jam_masuk`
 -- Struktur dari tabel `karyawan`
 --
 
-CREATE TABLE IF NOT EXISTS `karyawan` (
-`id_karyawan` int(11) NOT NULL,
+CREATE TABLE `karyawan` (
+  `id_karyawan` int(11) NOT NULL,
   `nip` varchar(255) NOT NULL,
   `nama` varchar(255) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data untuk tabel `karyawan`
 --
 
 INSERT INTO `karyawan` (`id_karyawan`, `nip`, `nama`) VALUES
-(1, '160700504', 'Duha'),
+(1, '160700504', 'Abdul'),
 (2, '12345678', 'Budi'),
 (3, '111222333', 'Siti');
 
@@ -70,31 +72,35 @@ INSERT INTO `karyawan` (`id_karyawan`, `nip`, `nama`) VALUES
 --
 
 --
--- Indexes for table `absensi`
+-- Indeks untuk tabel `absensi`
 --
 ALTER TABLE `absensi`
- ADD PRIMARY KEY (`id_absensi`), ADD KEY `id_karyawan` (`id_karyawan`);
+  ADD PRIMARY KEY (`id_absensi`),
+  ADD KEY `id_karyawan` (`id_karyawan`);
 
 --
--- Indexes for table `karyawan`
+-- Indeks untuk tabel `karyawan`
 --
 ALTER TABLE `karyawan`
- ADD PRIMARY KEY (`id_karyawan`);
+  ADD PRIMARY KEY (`id_karyawan`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `absensi`
+-- AUTO_INCREMENT untuk tabel `absensi`
 --
 ALTER TABLE `absensi`
-MODIFY `id_absensi` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=34;
+  MODIFY `id_absensi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+
 --
--- AUTO_INCREMENT for table `karyawan`
+-- AUTO_INCREMENT untuk tabel `karyawan`
 --
 ALTER TABLE `karyawan`
-MODIFY `id_karyawan` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+  MODIFY `id_karyawan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
